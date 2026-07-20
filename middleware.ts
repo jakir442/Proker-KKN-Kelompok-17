0cf21fb1c76b8d0e6aabff610a5a47bd38eff471
jakir442 <jakigeming2021@gmail.com>
2026-07-20 11:30:07 +0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,18 @@
-export default function middleware() {}
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { auth } from "@/auth";
+
+export default auth((req) => {
+    const isLoggedIn = !!req.auth;
+
+    const isDashboard = req.nextUrl.pathname.startsWith("/dashboard");
+
+    if (isDashboard && !isLoggedIn) {
+        return NextResponse.redirect(new URL("/login", req.url));
+    }
+
+    return NextResponse.next();
+});
 
 export const config = {
     matcher: ["/dashboard/:path*"],
