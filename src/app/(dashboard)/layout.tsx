4ac6fc029c99a8046f9d220281aba280654c97ef
jakir442 <jakigeming2021@gmail.com>
2026-07-20 14:33:58 +0700

    fix Updgrade tampilan Navbar Premium

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 import { Sidebar } from "./dashboard/super-admin/layout/Sidebar";
 import { Header } from "./dashboard/super-admin/layout/Header";
 import { Toaster } from "@/components/ui/sonner";
+import { NavigationItem } from "@/config/navigation";
 
 type DashboardLayoutProps = {
     children: ReactNode;
@@ -11,7 +12,7 @@ type DashboardLayoutProps = {
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
     return (
         <div className="min-h-screen bg-slate-100">
-            <Sidebar role="navigation" />
+            <Sidebar role={"navigation" as keyof NavigationItem[]} />
             <div className="ml-64 flex min-h-screen flex-col">
                 <Header />
                 <main className="flex-1 p-6">
